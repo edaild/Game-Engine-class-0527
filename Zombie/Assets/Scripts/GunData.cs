52b5d65e60f2452f018bfadd4c0956be46f6c595
edaild <ch5183sb@gmail.com>
2024-06-17 16:02:23 +0900

    Feat : 총알 발사 추가

--- Zombie/Assets/Scripts/GunData.cs
+++ Zombie/Assets/Scripts/GunData.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
-public class GunData
+
+[CreateAssetMenu(menuName = "Scriptable/Gundata", fileName ="Gundata")]
+
+public class GunData : ScriptableObject
 {
     public AudioClip shotClip; // 발사 소리
     public AudioClip reloadClip; // 재장전 소리
